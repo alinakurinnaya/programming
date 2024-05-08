701df910c24f16343f12e8f36e7ec8a476acd612
Alina <75946566+alinakurinnaya@users.noreply.github.com>
2024-05-08 23:43:45 +0300

    Create hw_1-2.cpp

---
+++ hw_1-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int squareSumOfDigits(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappyNumber(int n, std::unordered_set<int>& visited) {
+    if (n == 1) {
+        return true;
+    }
+
+    if (visited.find(n) != visited.end()) {
+        return false;
+    }
+
+    visited.insert(n);
+    int next = squareSumOfDigits(n);
+    return isHappyNumber(next, visited);
+}
+
+bool isHappy(int n) {
+    unordered_set<int> visited;
+    return isHappyNumber(n, visited);
+}
+
+int main() {
+    int number = 2;
+
+    if (isHappy(number)) {
+        cout << number << " - lucky number" << endl;
+    } else {
+        cout << number << " - unlucky number" << endl;
+    }
+
+    return 0;
+}
