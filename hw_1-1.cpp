c028afa4c77578f73e38604908ed9d01595b9dd7
Alina <75946566+alinakurinnaya@users.noreply.github.com>
2024-05-08 23:39:43 +0300

    Create hw_1-1.cpp

---
+++ hw_1-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9, 11, 13, 15};
+
+    int target = 7;
+    int index = binarySearch(arr, target);
+
+    if (index != -1) {
+        cout << "Element found at position: " << index << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+
+    return 0;
+}
